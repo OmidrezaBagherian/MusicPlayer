5257054bbc984fd729e4a80a40cc4a23587a0a55
OmidrezaBagherian <OmidCoder21@gmail.com>
2023-02-12 08:03:02 -0800

    Add files "fragment_list_all_music.xml" and "ListAllMusicFragment.kt" in project

---
+++ app/src/main/java/com/omidrezabagherian/musicplayer/ListAllMusicFragment.kt
@@ -0,0 +1,11 @@
+package com.omidrezabagherian.musicplayer
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+class ListAllMusicFragment : Fragment(R.layout.fragment_list_all_music) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}

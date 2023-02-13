6b54675e5305a6ba73f67522ae8ae51129c7165f
OmidrezaBagherian <OmidCoder21@gmail.com>
2023-02-13 02:10:11 -0800

    Add library "Navigation Component SafeArgs" & Config "MainActivity.kt"

--- app/src/main/java/com/omidrezabagherian/musicplayer/ui/main/MainActivity.kt
+++ app/src/main/java/com/omidrezabagherian/musicplayer/ui/main/MainActivity.kt
@@ -2,6 +2,8 @@ package com.omidrezabagherian.musicplayer.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import com.omidrezabagherian.musicplayer.R
 import com.omidrezabagherian.musicplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,8 +15,14 @@ class MainActivity : AppCompatActivity() {
 
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
 
-
+        setConfigNavigation()
 
         setContentView(mainBinding.root)
+    }
+
+    private fun setConfigNavigation() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment_main_host) as NavHostFragment
+        val navController = navHostFragment.navController
     }
 }

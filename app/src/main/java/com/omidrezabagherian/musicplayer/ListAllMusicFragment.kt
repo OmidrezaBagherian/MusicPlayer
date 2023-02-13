80688af9e967a572bcd2f4af7e045b89fc36f8c0
OmidrezaBagherian <OmidCoder21@gmail.com>
2023-02-12 22:56:37 -0800

    Add "viewBinding" and "dataBinding" in project

--- app/src/main/java/com/omidrezabagherian/musicplayer/ListAllMusicFragment.kt
+++ app/src/main/java/com/omidrezabagherian/musicplayer/ListAllMusicFragment.kt
@@ -3,9 +3,16 @@ package com.omidrezabagherian.musicplayer
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import com.omidrezabagherian.musicplayer.databinding.FragmentListAllMusicBinding
 
 class ListAllMusicFragment : Fragment(R.layout.fragment_list_all_music) {
+
+    private lateinit var listAllMusicBinding: FragmentListAllMusicBinding
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        listAllMusicBinding = FragmentListAllMusicBinding.bind(view)
+
         super.onViewCreated(view, savedInstanceState)
     }
 }
--- app/src/main/java/com/omidrezabagherian/musicplayer/PlayMusicFragment.kt
+++ app/src/main/java/com/omidrezabagherian/musicplayer/PlayMusicFragment.kt
@@ -3,9 +3,16 @@ package com.omidrezabagherian.musicplayer
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import com.omidrezabagherian.musicplayer.databinding.FragmentPlayMusicBinding
 
-class PlayMusicFragment:Fragment(R.layout.fragment_play_music) {
+class PlayMusicFragment : Fragment(R.layout.fragment_play_music) {
+
+    private lateinit var playMusicBinding: FragmentPlayMusicBinding
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        playMusicBinding = FragmentPlayMusicBinding.bind(view)
+
         super.onViewCreated(view, savedInstanceState)
     }
 }
